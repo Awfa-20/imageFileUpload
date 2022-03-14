19c7e4ed7755a37d23067431e10ff5d23a03d3fd
Aufa alkarfan <awfa.alkrfan@gmail.com>
2022-03-15 01:55:56 +0300

    adding home commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './_material/material.module';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './layouts/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
