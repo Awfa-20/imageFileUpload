d1526dc2cebf04e13172fbe3dc9ed9116a8a0609
Aufa alkarfan <awfa.alkrfan@gmail.com>
2022-06-16 03:13:08 +0300

    npm install @angular/core ng-chartist chartist --save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -9,6 +10,7 @@ const routes: Routes = [
 
   { path: '',canActivate:[!AuthGuard], component: SignInComponent },
   { path: 'sign-in',canActivate:[!AuthGuard], component: SignInComponent },
+
   {
     path: '',
     runGuardsAndResolvers: 'always',
@@ -16,6 +18,7 @@ const routes: Routes = [
     children: [
     { path: '', component: SignInComponent},
     { path: 'home', component: HomeComponent},
+    { path: '', component: DashboardComponent },
 
     ],
   },
